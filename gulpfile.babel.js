c608587e5616365c7f0c237b751219e5da70d35f
johnwatkins0 <johnwatkins0@gmail.com>
2017-04-18 11:58:45 -0400

    gulpfile cleanup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 
-gulp.task('default', () => {
-  return gulp
-    .src('src/**/*.js')
-    .pipe(babel({ presets: [ 'node6' ] }))
-    .pipe(gulp.dest('dist'));
-});
+gulp.task(
+  'default',
+  () =>
+    gulp
+      .src('src/**/*.js')
+      .pipe(babel({ presets: [ 'node6' ] }))
+      .pipe(gulp.dest('dist'))
+);
